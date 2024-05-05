539950b9c346848810b22886a588e16d916ed183
chainsawfever <162021726+chainsawfever@users.noreply.github.com>
2024-05-05 14:39:24 +0200

    Tranlation v02 (#2)
    * add translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * add spanish translations
    
    * create spanish compendium files and refactor register.js
    
    * add Mecánico abilitis to translations
    
    * add Mistico abilitis to translations
    
    * add abilities translations
    
    * add abilities and classes translations
    
    * add abilities and classes translations
    
    * add abilities class string and background translations
    
    * add crew habilities spanish translations
    
    * add crew habilities and reputation spanish translation
    
    * add crew types spanish translation
    
    * add crew types spanish translation
    
    * add background spanish translation
    
    * add classes spanish translation
    
    * add heritage spanish translation
    
    * add wanted spanish translation
    
    * add vice spanish translation
    
    * add ship size spanish translation
    
    * add system spanish translation
    
    * add system spanish translation
    
    * add upgrades  spanish translation
    
    * fix aux  upgrades  spanish translation
    
    * add items spanish translation
    
    * add items spanish translation
    
    * add items spanish translation
    
    ---------
    
    Co-authored-by: David Muñoz Muñoz <david@MacBook-Air-de-David.local>

--- register.js
+++ register.js
@@ -1,48 +1,13 @@
-const MODULE_ID = 'sav-translation-es';
+Hooks.once('init', () => {
 
-Hooks.on('init', () => {
-  game.settings.register(MODULE_ID, 'autoRegisterBabel', {
-    name: 'Automatically activate translation via Babele',
-    hint: 'Automatically implements Babele translations without needing to point to the directory containing the translations.',
-    scope: 'world',
-    config: true,
-    default: true,
-    type: Boolean,
-    onChange: value => {
-      if (value) {
-        autoRegisterBabel();
-      }
+	if(typeof Babele !== 'undefined') {
 
-      window.location.reload();
-    },
-  });
-
-  Babele.get().registerConverters({
-    "effectConverter": (effects, translations) => {
-      return effects.map(effect => {
-        if(translations) {
-          let translation = translations[effect._id];
-          if(translation) {
-            effect = mergeObject(effect, translation);
-          }
-        }
-        return effect;
-      });
-    }
-  });
-
-
-  if (game.settings.get(MODULE_ID, 'autoRegisterBabel')) {
-    autoRegisterBabel();
-  }
+		Babele.get().register({
+      module: 'sav-translation-es',
+      lang: 'es',
+      dir: 'compendium',
+		});
+	}
 });
 
-function autoRegisterBabel() {
-  if (typeof Babele !== 'undefined') {
-    Babele.get().register({
-      module: MODULE_ID,
-      lang: 'es',
-      dir: 'compendium/es',
-    });
-  }
-}
+
